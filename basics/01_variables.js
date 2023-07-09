8c6c93b14b1d05d60972123201ef45434c9311b1
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-07-09 14:21:48 +0000

    variables

---
+++ basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553; 
+let accountEmail= "nanananjsjs";
+var accountPassword = "12323";
+accountCity = "Bhilai";
+let accountState ;
+
+//accountId =2;//not allowed
+
+accountEmail = "hdjdkd";
+accountPassword = "2343334";
+accountCity = "Jaipur";
+
+// console.log(accountId);
+
+/*
+
+prefer not to use var because of issue with block scope and functional scope
+
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
