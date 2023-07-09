7e1d7b2d16166d4da6aee73b103a81d0bd9a4d83
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-07-09 14:41:33 +0000

    dataTypes

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3); // we are using node js not browser
+
+//code readability should be high
+
+let name = "Sankalp";
+let age = 18;
+let isLoggedIn = false;
+
+//number => 2 to  power 53 
+//bigInt => large number 
+//string => " "
+//boolean => true or false 
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//objects
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
