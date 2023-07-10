f6e6d09de6d0281a8567bb17ed54535619917f2c
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-07-10 05:56:43 +0000

    operations

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -2,12 +2,12 @@
 // let score =null;
 let score = undefined;
 
-console.log(typeof(score));
-console.log(typeof score);
+// console.log(typeof(score));
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33" => 33
 // "33abc" => NaN
@@ -24,7 +24,26 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber =33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********************** operations **************************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "hello";
+let str2 = " Sankalp";
+
+let str3 = str1 +str2;
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
 
 
