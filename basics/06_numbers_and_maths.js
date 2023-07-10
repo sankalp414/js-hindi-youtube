4aca0b5b1cf2077df298a03003971728c5b565b6
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-07-10 19:16:05 +0000

    numbers_and_maths

---
+++ basics/06_numbers_and_maths.js
@@ -0,0 +1,15 @@
+const score = 400;
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.89777;
+
+console.log(otherNumber.toPrecision(3));
+
+const hundereds = 1000000;
+
+console.log(hundereds.toLocaleString('en-IN'));
+
