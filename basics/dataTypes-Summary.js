e3ab29825ef0cc105f215434658e26036a79bf11
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-07-10 18:05:14 +0000

    dataType-Summary

---
+++ basics/dataTypes-Summary.js
@@ -0,0 +1,34 @@
+// #Primitive
+
+// 7 types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('124232');
+const anotherId = Symbol('124232')
+console.log(id === anotherId);
+
+
+
+//Refernce Type (Non-primitive)
+
+// Arrays, Objects, Functions
+
+
+const heroes = ['shakti', 'nag', 'doga'];
+
+let myObj = {
+    name : "sankalp",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Heloo world");
+}
+
+console.log(typeof outsideTemp);
