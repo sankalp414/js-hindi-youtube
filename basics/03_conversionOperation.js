b8bc644ed37e794ffebbac142344da6dc10bee8a
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-09-03 12:12:32 +0000

    operations

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -41,9 +41,19 @@ let str3 = str1 +str2;
 console.log(str3);
 
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
+console.log("1" + 2); //output will be 12
+console.log(1 + "2"); //output will be 12
+console.log("1" + 2 + 2); // output will be 122 because first thing is string so it consider all number as string
+console.log(1 + 2 + "2"); // output will be 32 because it starts with number therefore it treat it as a number and concat with the string 
 
+console.log(+true); //it shows very bad practice to code it should not be done and the output will be 1
+console.log(+""); //it also should not be done in professional area 
 
+let num1, num2,num3
+
+num1 = num2 = num3 = 2+2 //it produces confusion which is not good so it should not be done
+
+let gameCounter =100;
+gameCounter++;
+
+console.log(gameCounter); // the output will be 101
