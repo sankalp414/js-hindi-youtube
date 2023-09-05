c01e166da50a8528e278f72cfee7993d83f3647a
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-09-05 03:23:01 +0000

    math

--- basics/06_numbers_and_maths.js
+++ basics/06_numbers_and_maths.js
@@ -1,15 +1,33 @@
 const score = 400;
 const balance = new Number(100)
-console.log(balance);
+//console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(1));
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
 
 const otherNumber = 123.89777;
 
-console.log(otherNumber.toPrecision(3));
+//console.log(otherNumber.toPrecision(3));
 
 const hundereds = 1000000;
 
-console.log(hundereds.toLocaleString('en-IN'));
+//console.log(hundereds.toLocaleString('en-IN'));
 
+//++++++++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6)); // this not used generaly
+// console.log(Math.floor(4.6)); // this is not used generaly
+// console.log(Math.min(4,3,2,1)); 
+// console.log(Math.max(4,3,2,1)); 
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log((Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min +1))+ min);
