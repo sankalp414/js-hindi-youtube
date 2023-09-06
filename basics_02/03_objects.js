ea640fc5e56335d9a73f252bee318d9bef86ebdc
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-09-06 16:13:13 +0000

    objects

---
+++ basics_02/03_objects.js
@@ -0,0 +1,14 @@
+//singleton
+//Object.create
+
+//object literals
+
+const JsUser = {
+    name: "sankalp",
+    age: 21,
+    location: bhilai,
+    email: "snakalp@gmail.com",
+    isLoggedIn: false,
+    lastLogInDays: ["Monday", "Saturday"],
+}
+
