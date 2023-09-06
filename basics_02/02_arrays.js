4eee643b5bb04ac9b5cbde0b14a0a1559fc7339a
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-09-06 15:53:59 +0000

    arrays

--- basics_02/02_arrays.js
+++ basics_02/02_arrays.js
@@ -1,5 +1,27 @@
-const marvel_heroes = ["thor", "IronMan", "Spiderman"];
-const dc = ["superman", "flash", "batman"];
+const marvel_heroes = ["Thor", "IronMan", "SpiderMan"];
+const dc_heroes = ["SuperMan", "Flash", "BatMan"];
 
-marvel_heroes.push(dc);
+// marvel_heroes.push(dc_heroes);
+
 // console.log(marvel_heroes);
+// console.log(marvel_heroes[3][1]);
+
+// const all_heroes = marvel_heroes.concat(dc_heroes);
+
+// console.log(all_heroes);
+
+const all_new_heroes = [...marvel_heroes, ...dc_heroes];
+console.log(all_new_heroes);
+
+const anotherArray =[1,2,3,[4,5,6], [6,7, [4,5] ] ];
+
+const real_another_array = anotherArray.flat(Infinity);
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("sankalp"))
+console.log(Array.from("sankalp"));
+console.log(Array.from({name: "sankalp"}));
+
+
