f28d5f2590a76994ad292f8ce04938110d9452ec
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-09-07 14:42:11 +0000

    objects_part_1

--- basics_02/03_objects.js
+++ basics_02/03_objects.js
@@ -1,14 +1,24 @@
-//singleton
+//singleton it is made when we make object with a constructofr
 //Object.create
 
 //object literals
 
 const JsUser = {
     name: "sankalp",
+    "fullname": "sankalp sharma",
     age: 21,
-    location: bhilai,
+    location: "bhilai",
     email: "snakalp@gmail.com",
     isLoggedIn: false,
     lastLogInDays: ["Monday", "Saturday"],
 }
 
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["fullname"]);
+
+
+JsUser.email = "sankalp@1212344";
+Object.freeze(JsUser);
+JsUser.email = "dhdhhddh";
+console.log(JsUser);
