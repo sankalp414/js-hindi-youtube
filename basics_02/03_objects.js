8ea8b1d07d5fd514f6debb710e69a306c6716987
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-09-08 17:32:48 +0000

    objects

--- basics_02/03_objects.js
+++ basics_02/03_objects.js
@@ -1,24 +1,36 @@
-//singleton it is made when we make object with a constructofr
+//singleton it is made when we make object with a constructor
 //Object.create
 
 //object literals
-
+const mySym = Symbol("key1");
 const JsUser = {
     name: "sankalp",
-    "fullname": "sankalp sharma",
+   "fullname": "sankalp sharma", //it cannot be accessed by . notation so we have to access it by the help of [""]
     age: 21,
+    [mySym]: "myKey1",  //to use symbol as a key of the object we have to put the symbol under the []
+
     location: "bhilai",
     email: "snakalp@gmail.com",
     isLoggedIn: false,
-    lastLogInDays: ["Monday", "Saturday"],
+    lastLogInDays: ["Monday", "Saturday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser["fullname"]);
-
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["fullname"]);
+// console.log(JsUser[mySym]);
 
 JsUser.email = "sankalp@1212344";
-Object.freeze(JsUser);
-JsUser.email = "dhdhhddh";
+//Object.freeze(JsUser);
+JsUser.email = "shonty@123mfnf";
 console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello Js user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello Js user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
