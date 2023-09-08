3fafb93d9b0e457a399c183923abefbfa9235d35
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-09-08 17:50:29 +0000

    objects

--- basics_02/04_objects.js
+++ basics_02/04_objects.js
@@ -30,8 +30,31 @@ const obj2 = {
     4: "d"
 }
 
-// const obj3 = {obj1, obj2}
+// const obj3 = {obj1, obj2} we cannot do this because it will look like object is under another object
 
-const obj3 = Object.assign({},obj1, obj2)
+//const obj3 = Object.assign({},obj1, obj2) // this is the right way to do
 
-console.log(obj3);
+const obj3 = {...obj1, ...obj2};
+
+
+//console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email:"sankalpshrm1111@gmail.com"
+    },
+    {
+        id:1,
+        email:"sankalpshrm1111@gmail.com"
+    },
+    {
+        id:1,
+        email:"sankalpshrm1111@gmail.com"
+    }
+];
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
