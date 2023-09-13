98dd20c3a8ea662e2940f603742e8141e204f4c6
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-09-13 18:04:06 +0000

    numbers

--- basics/06_numbers_and_maths.js
+++ basics/06_numbers_and_maths.js
@@ -1,5 +1,5 @@
 const score = 400;
-const balance = new Number(10)
+const balance = new Number(10);
 //console.log(balance);
 
 //console.log(balance.toString().length);
@@ -28,6 +28,6 @@ console.log(Math.random()*10 + 1);
 console.log((Math.random()*10) + 1);
 
 const min = 10;
-const max = 20;
+const max = 200;
 
 console.log(Math.floor(Math.random() * (max-min +1))+ min);
