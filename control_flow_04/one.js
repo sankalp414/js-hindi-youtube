e816215ba5ce479123f10ccad8a1cf12eda49436
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-09-14 17:01:00 +0000

    control_flow

---
+++ control_flow_04/one.js
@@ -0,0 +1,51 @@
+// if 
+
+const isUserLoggedIn = true;
+
+const temperature =41;
+
+// if(temperature < 40){
+//     console.log("less then 50");
+// }else{
+//     console.log("tremeprature is greater then 50");
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+// const score = 200;
+
+// if(score> 100){
+//     const power = "fly";
+//     console.log(`User Power ${power}`);
+// }
+
+// const balance = 2000;
+
+// // if(balance>500) console.log("test");
+
+// if(balance < 500){
+//     console.log(`less then 500`);
+
+// }else if(balance < 750){
+//     console.log(`less then 750`);
+// }else if(balance < 900){
+//     console.log(`less then 900`);
+// }else {
+//     console.log(`less then 1200`);
+// }
+
+
+const userLoggedIn = true;
+const debitCard = true;
+
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if(userLoggedIn && debitCard && 2==4){
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("User is Logged in");
+}
---
+++ control_flow_04/truthy.js
@@ -0,0 +1,42 @@
+const userEmail = [];
+
+if(userEmail){
+    console.log("Got user Email");
+}else{
+    console.log("dont have userEmail");
+}
+
+// falsey values
+
+// flase, 0, -0, BigInt 0n,  "", null, undefined, NaN
+
+// truthy values
+
+// "0", 'false', " ", [], function(){}
+
+// if(userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+const emptyObj ={};
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("object is empty");
+}
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+
+
+console.log(val1);
+
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less then 80") : console.log("more then 80");;
