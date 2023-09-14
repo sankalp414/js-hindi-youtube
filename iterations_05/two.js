69c163913f58062b713f7f2462379c60e1ce3b63
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-09-14 18:11:39 +0000

    loops

---
+++ iterations_05/two.js
@@ -0,0 +1,21 @@
+
+let index = 0;
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index=index+2;
+// }
+
+let Array =['flash', 'superman', 'batman'];
+
+let arr= 0
+
+// while (arr<Array.length) {
+//     console.log(`Value is ${Array[arr]}`);
+//     arr= arr+1;
+// }
+
+let score = 11;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=10);
