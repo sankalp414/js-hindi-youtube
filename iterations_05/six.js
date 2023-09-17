eba4ac1af2495f94247bf7d5afddb33e72824198
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-09-17 17:47:55 +0000

    filter

---
+++ iterations_05/six.js
@@ -0,0 +1,27 @@
+// const coding = ['js', 'ruby', 'python', 'cpp'];
+
+// const values = coding.forEach((item)=>{
+//     // console.log(item);
+//     return item;
+// })
+
+// console.log(values);
+
+
+const myNums =[1,2,3,4,5,6,7,8,9,10];
+
+// let newNums= myNums.filter((num)=> { //always write return in this filter
+//     return num > 5
+// });
+// console.log(newNums);
+
+// const newNums =[];
+// myNums.forEach((num)=>{
+//     if (num>4) {
+//         newNums.push(num)
+        
+//     }
+// })
+
+// console.log(newNums);
+
