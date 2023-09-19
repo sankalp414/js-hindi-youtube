1e6bd4d806fda08ceeec4316726b132825db0972
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-09-19 17:29:07 +0000

    one

--- iterations_05/one.js
+++ iterations_05/one.js
@@ -5,6 +5,8 @@ for (let i = 0; i < 10; i++) {
     if(element == 5){
     //console.log("5 is best number");    //console.log(element);
     }
+
+    // the below is the nesting for loop
    for (let i = 0; i < array.length; i++) {
      for (let j = 0; j < array.length; j++) {
         const element = array[j];
