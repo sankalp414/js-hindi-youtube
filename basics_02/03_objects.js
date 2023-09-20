224906b391fd600757981ca693f3f651f00d6383
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-09-20 18:24:56 +0000

    objects

--- basics_02/03_objects.js
+++ basics_02/03_objects.js
@@ -2,12 +2,12 @@
 //Object.create
 
 //object literals
-const mySym = Symbol("key1");
+const mySym = Symbol("key1"); // here we declare a symbol
 const JsUser = {
-    name: "sankalp",
+    name: "sankalp", //by default all of this accesed as a string
    "fullname": "sankalp sharma", //it cannot be accessed by . notation so we have to access it by the help of [""]
     age: 21,
-    [mySym]: "myKey1",  //to use symbol as a key of the object we have to put the symbol under the []
+    [mySym]: "myKey1",  //to use symbol as a key of the object we have to put the symbol under the [] because otherwise it will be accesssed as a string
 
     location: "bhilai",
     email: "snakalp@gmail.com",
@@ -18,10 +18,10 @@ const JsUser = {
 // console.log(JsUser.email);
 // console.log(JsUser["email"]);
 // console.log(JsUser["fullname"]);
-// console.log(JsUser[mySym]);
+// console.log(JsUser[mySym]); // it can only be used by thsi way only 
 
 JsUser.email = "sankalp@1212344";
-//Object.freeze(JsUser);
+//Object.freeze(JsUser); // it freezes the object after ahich nothing can be changed in the object
 JsUser.email = "shonty@123mfnf";
 console.log(JsUser);
 
@@ -31,6 +31,6 @@ JsUser.greeting = function(){
 JsUser.greetingTwo = function(){
     console.log(`Hello Js user, ${this.name}`);
 }
-
-console.log(JsUser.greeting());
+ 
+console.log(JsUser.greeting());  
 console.log(JsUser.greetingTwo());
