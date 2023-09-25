e6adce949b4fdb06166c598f6d2a854bf738702d
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-09-25 17:44:08 +0000

    iterations

--- iterations_05/five.js
+++ iterations_05/five.js
@@ -18,7 +18,7 @@ const myNums =[1,2,3,4,5,6,7,8,9,10];
 // const newNums =[];
 // myNums.forEach((num)=>{
 //     if (num>4) {
-//         newNums.push(num)
+//         newNums.push(num) //it pushs the element to the array
         
 //     }
 // })
