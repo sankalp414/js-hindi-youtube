1ac78523fb53c1ebc81d72768bd7b6e953300137
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-10-05 19:27:59 +0000

    iterations

---
+++ iterations_05/six.js
@@ -0,0 +1,19 @@
+//const coding = ['js', 'ruby', 'python', 'cpp'];
+
+// const values = coding.forEach((item)=>{
+//     console.log(item);
+//     return item
+// }) // it doesnot returns the value 
+
+// console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+//const newNums=myNums.filter((num)=> num > 4 ); //in this we have call back function and in which we have a condition
+// and if we put the {} in the condition then it will return the empty object the you have to write the return 
+
+//console.log(newNums);
+
+
+
+
