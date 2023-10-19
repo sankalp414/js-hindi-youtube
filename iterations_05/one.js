d951f20438b79a753cc2cc93e7018456d8cb4aa5
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-10-19 18:09:14 +0000

    iterations

--- iterations_05/one.js
+++ iterations_05/one.js
@@ -9,7 +9,7 @@ for (let i = 0; i < 10; i++) {
     // the below is the nesting for loop
    for (let i = 0; i < array.length; i++) {
      for (let j = 0; j < array.length; j++) {
-        const element = array[j];
+        const element = array[j]; //element
         
      }
     
