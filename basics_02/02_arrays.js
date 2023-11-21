8652966d27b4b53fac491c12de1f30d07b0e823e
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-11-21 18:09:00 +0000

    basiccs

--- basics_02/02_arrays.js
+++ basics_02/02_arrays.js
@@ -28,6 +28,6 @@ console.log(Array.from({name: "sankalp"})); //interesting case because we have t
 
 let score1 = 100;
 let score2 = 200;
-let score3 = 300;
+let score3 = 600;
 
 console.log(Array.of(score1,score2,score3));
