035869b50030eecbe53469ba697a227e2b71e914
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2023-12-26 07:42:09 +0000

    async

--- async_07/function.js
+++ async_07/function.js
@@ -17,6 +17,7 @@ promiseOne.then(function(){  //.then has direct relation with resolve method in 
 new Promise(function(resolve,reject){
     setTimeout(function(){
         console.log("Async task 2");
+        resolve();
     },1000)
 }).then(function(){
     console.log("Async 2 resolved");
@@ -48,11 +49,11 @@ const promiseFour = new Promise(function(resolve,reject){
     },1000)
 })
 
-const username=promiseFour.then((user)=>{
+const username=promiseFour.then((user)=>{  //you cannot store the result in a variable we can resolve it by only implimenting again then it is known as chaining
     console.log(user);
     return user.username
 
-}).then((username)=>{
+}).then((username)=>{  //which ever value is in result is in the argument
     console.log(username);
 }).catch(function(error){
     console.log(error);
@@ -63,7 +64,7 @@ console.log(username);
 
 const promiseFive = new Promise(function(resolve, reject){
     setTimeout(function(){
-        let error = true;
+        let error = false;
         if(!error){
             resolve({username:"javascript", password: "123"})
         }else{
@@ -74,9 +75,23 @@ const promiseFive = new Promise(function(resolve, reject){
 });
 
 async function consumePromiseFive(){
-    const response = await promiseFive;
-    console.log(response); 
+    try {
+        const response = await promiseFive;
+        console.log(response); 
+        
+    } catch (error) {
+        console.log(error);
+    }
 }    
 consumePromiseFive()
 
-fetch("")
+async function getAllUser(){ 
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data =  await response.json();
+        console.log(data);
+    }catch(error){
+        console.log('E:', error);
+    }
+}
+getAllUser()
