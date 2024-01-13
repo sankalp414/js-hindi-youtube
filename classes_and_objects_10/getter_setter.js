ba53b3b1899bcd59723d5b64c3440ff8e7e28e12
sankalp414 <83761353+sankalp414@users.noreply.github.com>
2024-01-13 18:46:03 +0000

    getter_setter

---
+++ classes_and_objects_10/getter_setter.js
@@ -0,0 +1,12 @@
+class User{
+    constructor(email,password){
+        this.email=email;
+        this.password = password
+    }
+    get password(){
+        return this.password.toUpperCase()
+    }
+}
+
+const hitesh = new User("@hitesh.ai","123")
+console.log(hitesh.password);
